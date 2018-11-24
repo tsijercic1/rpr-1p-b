32a91fa9fc420e58a327ad72691c2e1be90583cd
tsijercic1 <tsijercic1@etf.unsa.ba>
2018-11-24 15:31:49 +0100

    Changes

---
+++ src/ba/unsa/etf/rpr/p1/AnalogniAparat.java
@@ -0,0 +1,7 @@
+package ba.unsa.etf.rpr.p1;
+
+public class AnalogniAparat extends Aparat {
+    public AnalogniAparat(String sifra, int serijski) {
+        super(sifra, serijski);
+    }
+}
---
+++ src/ba/unsa/etf/rpr/p1/DigitalniAparat.java
@@ -0,0 +1,8 @@
+package ba.unsa.etf.rpr.p1;
+
+public class DigitalniAparat extends Aparat {
+    public DigitalniAparat(String sifra, int serijski) {
+        super(sifra, serijski);
+    }
+
+}
